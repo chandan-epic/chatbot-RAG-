9bccea43708909c10b20be9a9c935f13c9a84df4
Manohar <manu.nimmala124@gmail.com>
2024-07-19 12:57:36 +0530

    add vectordbuploadfile

--- backend/controller/createbotController.js
+++ backend/controller/createbotController.js
@@ -1,8 +1,10 @@
+const {uploadIntoVectorDb}=require("../createdb")
+
 function createBot(req,res){
 
      console.log(req.file);
      console.log(req.body);
- 
+    
      if (!req.file) {
          return res.status(400).json({ message: 'No file uploaded' });
      }
